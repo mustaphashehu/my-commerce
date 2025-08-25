a61f534de17f5e28601f9c850a52353d31f11065
coding-major <shehumustapham@gmail.com>
2025-08-25 23:58:59 +0100

    port baseurl fixed

--- client/src/utils/service.js
+++ client/src/utils/service.js
@@ -1,5 +1,5 @@
 // export const baseUrl = "http://localhost:4000/api/v1"
-export const baseUrl = "https://e-commerce-c2t6.onrender.com"
+export const baseUrl = "https://e-commerce-c2t6.onrender.com/api/v1"
 
 export const postRequest = async(url, body) => {
     
